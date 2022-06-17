dd5d3f4cf64b3d3a00adb00acd6e6096573852d7
Anthony Bronca <95654116+AnthonyBronca@users.noreply.github.com>
2022-06-17 15:00:44 -0400

    fixed log out bug

--- frontend/src/components/EditTweetForm/EditTweetForm.js
+++ frontend/src/components/EditTweetForm/EditTweetForm.js
@@ -20,7 +20,7 @@ function EditTweetForm() {
     const [errors, setErrors] = useState([])
     const [maxLength, setMaxLength] = useState([])
 
-    useEffect(()=> {
+    useEffect(() => {
         let validations = [];
         let customMessage = [];
         if (tweetField.length > 280) validations.push('Tweet can not be longer than 280 Characters');
@@ -35,7 +35,7 @@ function EditTweetForm() {
         e.stopPropagation();
         const validations = [];
 
-        if(tweetField.length === 0 ){
+        if (tweetField.length === 0) {
             validations.push('You can not submit a blank reply.')
             setErrors(validations);
             return;
@@ -44,7 +44,7 @@ function EditTweetForm() {
         const tweetId = id
         dispatch(updateTweetThunk(tweetId, tweetField))
             .then(() => history.push(`/tweets/${tweetId}`))
-            .then(()=> dispatch(getTweetsThunk()))
+            .then(() => dispatch(getTweetsThunk()))
 
         //     const userId = authorizedUser.id
         //     const form = {
@@ -56,39 +56,39 @@ function EditTweetForm() {
         // }
     }
 
-    function sendEmBack(e){
+    function sendEmBack(e) {
         history.push(`/tweets/${id}`)
     }
 
-    if (authorizedUser.id !== tweet?.userId) {
-        history.push(`/tweets/${id}`)
-    } else {
-        return (
-            <form onSubmit={handleSubmit}>
-                <div className='edit-form-container'>
-                    <label> Edit Tweet Body:
-                        <textarea className='edit-tweet-body-input'
-                            placeholder={tweet?.tweet}
-                            value={tweetField}
-                            maxLength={280}
-                            onChange={e => setTweetField(e.target.value)}></textarea>
-                    </label>
-                    <div className='submit-edit-button'>
-                        <div className='button-holder'>
+    // if (authorizedUser.id !== tweet?.userId) {
+    //     history.push(`/tweets/${id}`)
+    // } else {
+    return (
+        <form onSubmit={handleSubmit}>
+            <div className='edit-form-container'>
+                <label> Edit Tweet Body:
+                    <textarea className='edit-tweet-body-input'
+                        placeholder={tweet?.tweet}
+                        value={tweetField}
+                        maxLength={280}
+                        onChange={e => setTweetField(e.target.value)}></textarea>
+                </label>
+                <div className='submit-edit-button'>
+                    <div className='button-holder'>
                         <button className='new-tweet-button-one-tweet'>Submit Edit</button>
-                        </div>
-                        <div className='go-back-holder'>
-                            <button className='new-tweet-button-one-tweet' onClick={e=> sendEmBack(e)}>Go Back</button>
-                        </div>
-                        <div className='message-holder'>
-                        {maxLength? maxLength.map(msg => <li>{msg}</li>): errors? errors.map(error => <li>{error}</li>): null}
-                        {errors? errors.map(error => <li>{error}</li>):null}
-                        </div>
+                    </div>
+                    <div className='go-back-holder'>
+                        <button className='new-tweet-button-one-tweet' onClick={e => sendEmBack(e)}>Go Back</button>
+                    </div>
+                    <div className='message-holder'>
+                        {maxLength ? maxLength.map(msg => <li>{msg}</li>) : errors ? errors.map(error => <li>{error}</li>) : null}
+                        {errors ? errors.map(error => <li>{error}</li>) : null}
                     </div>
                 </div>
-            </form>
-        )
-    }
+            </div>
+        </form>
+    )
 }
+// }
 
 export default EditTweetForm
